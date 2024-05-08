ad3bf0af9acecaf3fca90d48e7d3631534d2f8be
romainbianchi <104493944+romainbianchi@users.noreply.github.com>
2024-05-08 14:01:39 +0200

    Added images in manualTree

--- src/views/Tree/TreeManual.tsx
+++ src/views/Tree/TreeManual.tsx
@@ -6,6 +6,23 @@ interface TreeManualProps {
     lookUpTableCallback: () => void; // Define the type of lookUpTableCallback
 }
 
+// Correspondances between actions/conditions and images
+const actCondImages = {
+    'FORWARD': 'public/forward.png',
+    'BACKWARD': 'public/backward.png',
+    'LEFT': 'public/left.png',
+    'RIGHT': 'public/right.png',
+    'STOP': 'public/stop.png',
+    'sensor0': 'public/sensor0.png',
+    'sensor1': 'public/sensor1.png',
+    'sensor2': 'public/sensor2.png',
+    'sensor3': 'public/sensor3.png',
+    'sensor4': 'public/sensor4.png',
+    'sensor5': 'public/sensor5.png',
+    'sensor6': 'public/sensor6.png',
+    'Condition or Action': 'public/transparent.png',
+}
+
 
 
 /* adapted from React-Node-Flow: https://github.com/kumarabhishek008/React-Node-Flow/tree/master */
@@ -17,7 +34,9 @@ const manualTreeRendering = (ManualTreeData: any, handleOnDropInTree:(e:React.Dr
           {
               ManualTreeData.map((item: any)=>
                   <li key={item.id} className={item.text+item.id}>
-                      <div data-id={item.id} onDrop={(e) => handleOnDropInTree(e)} onDragOver={(e) => e.preventDefault()}>{item.text}</div>
+                      <div data-id={item.id} onDrop={(e) => handleOnDropInTree(e)} onDragOver={(e) => e.preventDefault()}>
+                        <img src={actCondImages[item.text as keyof typeof actionImage]} alt={item.text} width="75%" height="100%" />
+                      </div>
                       {
                           item.children && item.children.length ?
                           manualTreeRendering(item.children, handleOnDropInTree)
